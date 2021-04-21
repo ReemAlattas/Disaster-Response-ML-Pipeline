4d8c35d4a40d514b627a7efe2d0b76face61d14a
Reem <ralattas@live.com>
2021-04-21 15:50:31 +0300

    Use replace if exists

--- data/process_data.py
+++ data/process_data.py
@@ -86,7 +86,7 @@ def save_data(df, database_filename):
     '''
 
     engine = create_engine('sqlite:///{}'.format(database_filename))
-    df.to_sql('LabeledMessages', engine, index=False)
+    df.to_sql('LabeledMessages', engine, if_exists='replace', index=False)
 
 
 def main():
